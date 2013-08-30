b2c90bca8ff5781936328740146faf2ee7c04328
Tarang <tarangrockr@gmail.com>
2013-08-30 20:42:03 +0530

    javacsript file modified

--- Assets/js/brainvita-game.js
+++ Assets/js/brainvita-game.js
@@ -1,30 +1,48 @@
+
+
+var coinStatus = new Array(
+                           new Array(-1,-1,1,1,1,-1,-1),
+                           new Array(-1,-1,1,1,1,-1,-1),
+                           new Array(1,1,1,1,1,1,1),
+                           new Array(1,1,1,0,1,1,1),
+                           new Array(1,1,1,1,1,1,1),
+                           new Array(-1,-1,1,1,1,-1,-1),
+                           new Array(-1,-1,1,1,1,-1,-1)
+                          );
+              
+
+
 function startGame() {
+            loadCoins();
+            
+}
+
+function loadCoins() {
             var data = "<table cellpadding= 3 ; cellspacing = 1>";
             for(var i=0;i<7;i++) {
                 data=data+"<tr>";
                 
                 for(var j=0;j<7;j++){
                     data=data+"<td>";
-                        if (i<2 && j>=2 && j<=4) {
-                            data=data+"<a id= '"+i+j+"' href='#'><img src='Assets/images/coin.png' onclick= 'return coinClicked("+i+","+j+")'/> </a>";
+                        var x = coinStatus[i][j];
+            
+                        if (x == 1) {
+                           data=data+"<a id= '"+i+j+"' href='#'><img src='Assets/images/coin.png' onclick= 'return coinClicked("+i+","+j+")'/> </a>";
                         }
-                        if (i==3 && j==3) {
+                        if (x == 0) {
                             data=data+"<a id= '"+i+j+"' href='#'><img src='Assets/images/empty.png' onclick= 'return coinClicked("+i+","+j+")'/> </a>";
                         }
-                        else if(i>=2 && i<=4) {
-                            data=data+"<a id= '"+i+j+"' href='#'><img src='Assets/images/coin.png' onclick= 'return coinClicked("+i+","+j+")'/> </a>";
-                        }
-                        if (i>=5 && j>=2 && j<=4 ) {
-                            data=data+"<a id= '"+i+j+"' href='#'><img src='Assets/images/coin.png' onclick= 'return coinClicked("+i+","+j+")'/> </a>";
-                        }
+                        
                     data=data+"</td>";
                     }
                     data=data+"</tr>";
             }
             data+="</table>";
             document.getElementById("gameBoard").innerHTML = data;
-        }
+            
         
+}
+
 function coinClicked(x,y){
     
 }
